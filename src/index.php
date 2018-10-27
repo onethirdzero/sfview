2a76868fe43e271f5b70cd04a759bfa85b582cf7
Curtis Babnik <curtisbabnik@gmail.com>
2018-10-27 02:28:04 -0700

    Reorganize index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="no-js" lang="en">
+<html class="no-js" lang="en" style="width: 100%; height: 100%;">
 
 <head>
   <meta charset="utf-8">
@@ -15,28 +15,35 @@
 
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
-</head>
+  <link rel="stylesheet" href="css/vendor/photo-sphere-viewer.css">
+  <link rel="stylesheet" href="css/vendor/bootstrap.css">
 
-<body>
-  <!--[if lte IE 9]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-  <![endif]-->
-
-  <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
-  <a href="/html/main.html">Main.html</a>
+  <script src="js/vendor/photo-sphere-viewer.js"></script>
+  <script src="js/vendor/bootstrap.js"></script>
   <script src="js/vendor/modernizr-3.6.0.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
   <script src="js/plugins.js"></script>
   <script src="js/main.js"></script>
-
   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
   <script>
       window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
       ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
   </script>
   <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</head>
+
+<?php include("php/nav-bar.php") ?>
+
+<body style="width: 100%; height: 100%;">
+<!--[if lte IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+<![endif]-->
+
+<h2>I uh, broke the panorama when I was making things not so hackish</h2>
+<div class="panorama" id="pan1" style="width: 100%; height: 100%;"></div>
+<script> setPanorama("pan1", "img/stock_nature.jpg") </script>
+
 </body>
 
 </html>
