80aa283d62ecd7df929681a45068e4eb60139ca1
Curtis Babnik <curtisbabnik@gmail.com>
2018-10-27 02:29:22 -0700

    Update latest dist/ contents

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+function setPanorama(div_id, url){
+  let div = document.getElementById(div_id);
+
+  let params = {
+    container: div,
+    panorama: url,
+  };
+
+  //return new PhotoSphereViewer(params);
+}
