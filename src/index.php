2a69ec7c41b25947e630087a60a2a8201811f7a4
Curtis Babnik <curtisbabnik@gmail.com>
2018-10-27 18:53:47 -0700

    Move photosphere logic back into main.js

--- src/index.php
+++ src/index.php
@@ -18,22 +18,22 @@
   <link rel="stylesheet" href="css/vendor/photo-sphere-viewer.css">
   <link rel="stylesheet" href="css/vendor/bootstrap.css">
 
-  <script src="js/vendor/uevent.js"></script>
-  <script src="js/vendor/three.js"></script>
-  <script src="js/vendor/doT.js"></script>
-  <script src="js/vendor/D.js"></script>
-  <script src="js/vendor/photo-sphere-viewer.js"></script>
-  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script> <!--Photosphere logic should go inside here...-->
+  <script type="text/javascript" src="js/vendor/uevent.js"></script>
+  <script type="text/javascript" src="js/vendor/three.js"></script>
+  <script type="text/javascript" src="js/vendor/doT.js"></script>
+  <script type="text/javascript" src="js/vendor/D.js"></script>
+  <script type="text/javascript" src="js/vendor/photo-sphere-viewer.js"></script>
+  <script type="text/javascript" src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script type="text/javascript" src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script type="text/javascript" src="js/plugins.js"></script>
+  <script type="text/javascript" src="js/main.js"></script>
   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
   <script>
       window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
       ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
   </script>
-  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+  <script type="text/javascript" src="https://www.google-analytics.com/analytics.js" async defer></script>
 </head>
 
 <?php include("php/nav-bar.php") ?>
@@ -44,22 +44,7 @@
 <![endif]-->
 
 <div class="panorama" id="pan1" style="width: 100%; height: 100%;"></div>
-
-<!-- Photosphere Logic: This should be moved to an external js file..
-Currently experiencing problems with linking to Photo Sphere Viewer, 
-so we're putting this here for now (see main.js )-->
-<script type="text/javascript">
-  function setPanorama2(div_id, url){
-    let div = document.getElementById(div_id);
-    let params = {
-      container: div,
-      panorama: url,
-    };
-
-    return new PhotoSphereViewer(params);
-  }
-</script>
-<script> setPanorama2("pan1", "img/stock_nature.jpg") </script> 
+<script> setPanorama("pan1", "img/stock_nature.jpg") </script>
 
 </body>
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,11 @@
-// THIS IS NOT WORKING!!
-// do not expect setPanorama to do anything
-// the problem lays in getting the word 'PhotoSphereViewer' recognized
-
-import 'vendor/uevent.js';
-import 'vendor/three.js';
-import 'vendor/doT.js';
-import 'vendor/D.js';
-import {PhotoSphereViewer} from 'vendor/photo-sphere-viewer.js';
+/* eslint no-undef: "off" */
 
 function setPanorama(div_id, url){
-  alert("inside setPanorama");
-  let div = document.getElementById(div_id);
-  console.log(div.className);
+    let div = document.getElementById(div_id);
+    let params = {
+        container: div,
+        panorama: url,
+    };
 
-  let params = {
-    container: div,
-    panorama: url
-  };
-
-  return new PhotoSphereViewer(params); // PhotoSphereViewer is not recognized
+    return new PhotoSphereViewer(params);
 }
